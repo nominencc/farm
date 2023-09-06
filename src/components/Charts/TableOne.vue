75ef5b52cf20c6d997d53bce9988f1312d439760
nominencc <13368043554@163.com>
2023-09-06 17:32:57 +0800

    时间补0

--- src/components/Charts/TableOne.vue
+++ src/components/Charts/TableOne.vue
@@ -58,10 +58,10 @@ export default{
         getCurrentTime() {
             const now = new Date();
             const year = now.getFullYear();
-            const month = now.getMonth();
-            const day = now.getDate();
-            const hours = now.getHours();
-            const minutes = now.getMinutes();
+            const month = String(now.getMonth()).padStart(2,'0');
+            const day = String(now.getDate()).padStart(2,'0');
+            const hours = String(now.getHours()).padStart(2,'0');
+            const minutes = String(now.getMinutes()).padStart(2,'0');
             this.Time = `${year}-${month}-${day} ${hours}:${minutes}`;
         }
     },
